921aa74618d52017761a2eadafaf99722939b4b3
Kat Buchanan <katrbuch@gmail.com>
2021-05-07 13:51:08 -0500

    updated model

--- models/book.js
+++ models/book.js
@@ -1,13 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  id: { type: String, require: true },
+  title: { type: String, require: true },
+  authors: [{ type: String, require: true }],
+  description: { type: String, require: true },
+  image: { type: String, unique: true, dropDups: true },
+  link: { type: String, required: true },
 });
 
-const Book = mongoose.model("Book", bookSchema);
+const Book = mongoose.model('Book', bookSchema);
 
 module.exports = Book;
