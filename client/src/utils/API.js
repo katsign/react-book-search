5a11c142cb7964f0816349e0a6b4f6e8176ddff3
Kat Buchanan <katrbuch@gmail.com>
2021-05-09 08:33:04 -0500

    client routes

---
+++ client/src/utils/API.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const API = {
+  // Retrieves Google books
+  googleBooks: function (query) {
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+  },
+  // Saves book to the DB
+  saveBook: function (bookData) {
+    return axios.post('/api/books', bookData);
+  },
+  // Gets saved books from DB
+  getBooks: function () {
+    return axios.get('/api/books');
+  },
+  // Gets book with the given ID
+  getBook: function (id) {
+    return axios.get('/api/books/' + id);
+  },
+  // Deletes book with the given ID
+  deleteBook: function (id) {
+    return axios.delete('/api/books/' + id);
+  },
+};
+
+export default API;
