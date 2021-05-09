eb11735fa74e575cbc1b9c23e29e7822b579328f
Kat Buchanan <katrbuch@gmail.com>
2021-05-09 13:56:25 -0500

    header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <>
+      <nav className="navbar navbar-light justify-content-between">
+        <a className="navbar-brand" href="/">
+          Home
+        </a>
+        <a className="navbar-brand" href="/saved">
+          Saved Books
+        </a>
+      </nav>
+      <div className="text-center">
+        <hr />
+        <h2>Google Books Search 📚</h2>
+      </div>
+    </>
+  );
+}
+
+export default Header;
