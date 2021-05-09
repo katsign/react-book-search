f05b6e6582348583d64386236132acdd8bd092e3
Kat Buchanan <katrbuch@gmail.com>
2021-05-09 10:07:30 -0500

    handle clear + fixes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -32,6 +32,11 @@ class Home extends Component {
     this.searchBooks();
   };
 
+  handleClear = (event) => {
+    event.preventDefault();
+    this.setState({ books: [], search: '' });
+  };
+
   saveGoogleBook = (currentBook) => {
     console.log('CURRENT BOOK ==>', currentBook);
     API.saveBook({
@@ -51,28 +56,38 @@ class Home extends Component {
       <div className="container">
         <div className="row" style={{ margin: 'auto' }}>
           <div className="col-xs-12">
-            <h4>Book Search 📚</h4>
-            <form>
-              <div className="input-group mb-2">
-                <input
-                  type="text"
-                  className="form-control mr-2"
-                  placeholder="Twilight"
-                  aria-label="search" aria-describedby="searchBtn"
-                  name="search"
-                  onChange={this.handleInputChange}
-                  value={this.state.location}
-                />
-                <button
-                  onClick={this.handleFormSubmit}
-                  className="btn btn-primary"
-                  type="submit"
-                  id="searchBtn"
-                >
-                  Search
-                </button>
-              </div>
-            </form>
+            <h4>Google Books Search 📚</h4>
+            <div className="d-flex flex-row align-items-center mb-2">
+              <form>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Twilight"
+                    aria-label="search"
+                    aria-describedby="searchBtn"
+                    name="search"
+                    onChange={this.handleInputChange}
+                    value={this.state.search}
+                  />
+                  <button
+                    onClick={this.handleFormSubmit}
+                    className="btn btn-primary"
+                    type="submit"
+                    id="searchBtn"
+                  >
+                    Search
+                  </button>
+                </div>
+              </form>
+              <button
+                onClick={this.handleClear}
+                style={{ marginLeft: '0.5rem' }}
+                className="btn btn-primary"
+              >
+                Clear
+              </button>
+            </div>
 
             {this.state.books.length ? (
               <Results
