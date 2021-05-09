abe94b3474fec6d70070831d5ada15b4873b9b9e
Kat Buchanan <katrbuch@gmail.com>
2021-05-09 13:58:15 -0500

    nesting components

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './utils/index.css';
-import Home from './pages/Home'
+import Home from './pages/Home';
+import Header from './components/Header';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </Router>
+    <div className="container">
+      <div className="row" style={{ margin: 'auto' }}>
+      <Header />
+        <div className="col-xs-12">
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </Router>
+        </div>
+      </div>
+    </div>
   );
 }
 
