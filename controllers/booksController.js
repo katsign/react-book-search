2bc38e1eef24a5f07078aa2ea474c1b0fd485f13
Kat Buchanan <katrbuch@gmail.com>
2021-05-09 12:30:58 -0500

    updated controller

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -11,19 +11,13 @@ module.exports = {
   },
   findById: function(req, res) {
     db.Book
-      .findById(req.params.id)
+      .findById({id: req.params.id})
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  create: function(req, res) {
+  save: function(req, res) {
     db.Book
       .create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  update: function(req, res) {
-    db.Book
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
