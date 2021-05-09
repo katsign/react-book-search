ab523f762276a9071d4a9d5bc6fe6784ba95eeca
Kat Buchanan <katrbuch@gmail.com>
2021-05-09 14:54:50 -0500

    save item

---
+++ client/src/components/Save.js
@@ -0,0 +1,43 @@
+function Save(props) {
+  const { title, authors, image, link, description, deleteSavedBook } = props;
+  return (
+    <div className="card-group">
+      <div className="card mb-2">
+        <div className="d-flex flex-row align-items-center justify-content-between card-header">
+          <div>
+            <h5 className="card-title">{title}</h5>
+            <small>
+              <span className="material-icons" style={{ fontSize: '20px' }}>
+                menu_book
+              </span>
+              {authors}
+            </small>
+          </div>
+          <img src={image} style={{ maxWidth: '100px' }} alt="book" />
+        </div>
+        <div className="card-body">
+          <small className="card-text">{description}</small>
+        </div>
+        <div className="card-footer">
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-primary"
+          >
+            View Book
+          </a>
+          <button
+            onClick={deleteSavedBook.bind(this, props)}
+            className="btn btn-outline-danger"
+            style={{ marginLeft: '0.5rem' }}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Save;
