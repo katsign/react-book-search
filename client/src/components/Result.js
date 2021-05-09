e39d91948aa95d790579b850f5517ea25eb39e5a
Kat Buchanan <katrbuch@gmail.com>
2021-05-09 08:34:11 -0500

    result component

---
+++ client/src/components/Result.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+
+class Result extends Component {
+  state = {
+    mounted: false,
+    bgColor: '',
+    color: '',
+    text: 'Save',
+  };
+
+  componentDidMount = () => {
+    this.setState({
+      mounted: true,
+    });
+  };
+
+  setSave = () => {
+    if (this.state.text === 'Save') {
+      this.setState({
+        bgColor: '#b6c1cf',
+        color: 'white',
+        text: 'Saved',
+      });
+    } else {
+      this.setState({
+        bgColor: '',
+        color: '',
+        text: 'Save',
+      });
+    }
+  };
+
+  onClickSave = () => {
+    this.props.saveGoogleBook(this.props);
+    this.setSave();
+  };
+
+  render() {
+    // eslint-disable-next-line no-unused-vars
+    const { book } = this.props;
+
+    return (
+      <div className="card-group">
+        <div className="card mb-2">
+          <div className="d-flex flex-row align-items-center justify-content-between card-header">
+            <div>
+              <h5 className="card-title">{this.props.title}</h5>
+              <small>
+                <span className="material-icons" style={{fontSize: '20px'}}>menu_book</span>{' '}
+                {this.props.authors}
+              </small>
+            </div>
+            <img
+              src={this.props.image}
+              style={{ maxWidth: '100px' }}
+              alt="book"
+            />
+          </div>
+          <div className="card-body">
+            <small className="card-text">{this.props.description}</small>
+          </div>
+          <div className="card-footer">
+            <a
+              href={this.props.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn btn-primary"
+            >
+              View Book
+            </a>
+            <button
+              onClick={this.onClickSave}
+              style={{
+                backgroundColor: this.state.bgColor,
+                color: this.state.color,
+              }}
+              className="btn"
+            >
+              {this.state.text}
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Result;
