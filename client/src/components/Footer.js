389d1030a46dd1102aee2bd70f32630e23ccd7ef
Kat Buchanan <katrbuch@gmail.com>
2021-05-09 13:55:54 -0500

    footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="mt-2 text-center">
+      <hr></hr>
+      <a href="#top">
+        Back to Top
+      </a>
+      <span className="material-icons fill" style={{fontSize: '20px'}}>north</span>
+    </div>
+  );
+}
+
+export default Footer;
